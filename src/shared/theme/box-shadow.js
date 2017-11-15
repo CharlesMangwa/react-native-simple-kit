5f96a68234d4030c0d7e7760f48967b5e3cbb5ca
Charles Mangwa <charlesmangwa@gmail.com>
2017-11-15 19:18:57 +0100

    refactor(shared): update content

--- src/shared/theme/box-shadow.js
+++ src/shared/theme/box-shadow.js
@@ -1,9 +1,17 @@
 /* @flow */
 
+import { Platform } from 'react-native'
+
 export const DEFAULT_BOX_SHADOW: Object = {
-  shadowColor: 'black',
-  shadowOffset: { width: 0, height: 5 },
-  shadowOpacity: 0.3,
-  shadowRadius: 1,
-  elevation: 2,
+  ...Platform.select({
+    android: {
+      elevation: 3,
+    },
+    ios: {
+      shadowColor: '#3F3F3F',
+      shadowOffset: { width: 0, height: 6 },
+      shadowOpacity: 0.3,
+      shadowRadius: 10,
+    },
+  }),
 }
--- src/shared/theme/colors.js
+++ src/shared/theme/colors.js
@@ -1,30 +1,32 @@
 /* @flow */
 
+type Color = string
+
 // Neutral tones - Grey
-export const NEUTRAL_COLOR_00: string = '#fefefe'
-export const NEUTRAL_COLOR_10: string = '#f2f2f2'
-export const NEUTRAL_COLOR_15: string = '#f0f0f0'
-export const NEUTRAL_COLOR_20: string = '#ebebeb'
-export const NEUTRAL_COLOR_30: string = '#dddddd'
-export const NEUTRAL_COLOR_35: string = '#d8d8d8'
-export const NEUTRAL_COLOR_40: string = '#cdcdcd'
-export const NEUTRAL_COLOR_45: string = '#c8c8c8'
-export const NEUTRAL_COLOR_50: string = '#b8b8b8'
-export const NEUTRAL_COLOR_55: string = '#8e8e8e'
-export const NEUTRAL_COLOR_70: string = '#4A4A4A'
-export const NEUTRAL_COLOR_80: string = '#333333'
-export const NEUTRAL_COLOR_85: string = '#1F1F1F'
-export const NEUTRAL_COLOR_90: string = '#171719'
-export const NEUTRAL_COLOR_95: string = '#121214'
-export const NEUTRAL_COLOR_100: string = '#121212'
+export const NEUTRAL_COLOR_00: Color = '#fefefe'
+export const NEUTRAL_COLOR_10: Color = '#f2f2f2'
+export const NEUTRAL_COLOR_15: Color = '#f0f0f0'
+export const NEUTRAL_COLOR_20: Color = '#ebebeb'
+export const NEUTRAL_COLOR_30: Color = '#dddddd'
+export const NEUTRAL_COLOR_35: Color = '#d8d8d8'
+export const NEUTRAL_COLOR_40: Color = '#cdcdcd'
+export const NEUTRAL_COLOR_45: Color = '#c8c8c8'
+export const NEUTRAL_COLOR_50: Color = '#b8b8b8'
+export const NEUTRAL_COLOR_55: Color = '#8e8e8e'
+export const NEUTRAL_COLOR_70: Color = '#4A4A4A'
+export const NEUTRAL_COLOR_80: Color = '#333333'
+export const NEUTRAL_COLOR_85: Color = '#1F1F1F'
+export const NEUTRAL_COLOR_90: Color = '#171719'
+export const NEUTRAL_COLOR_95: Color = '#121214'
+export const NEUTRAL_COLOR_100: Color = '#121212'
 
 // Default colors
-export const DEFAULT_TEXT_COLOR: string = NEUTRAL_COLOR_00
-export const DEFAULT_TEXT_COLOR_DARK: string = NEUTRAL_COLOR_80
-export const DEFAULT_ICON_COLOR: string = NEUTRAL_COLOR_35
-export const DEFAULT_BACKGROUND_COLOR: string = NEUTRAL_COLOR_10
-export const SUCCESS_COLOR: string = '#53C000'
-export const WARNING_COLOR: string = '#FFAD1D'
-export const FAIL_COLOR: string = '#E33258'
+export const DEFAULT_TEXT_COLOR: Color = NEUTRAL_COLOR_00
+export const DEFAULT_TEXT_COLOR_DARK: Color = NEUTRAL_COLOR_80
+export const DEFAULT_ICON_COLOR: Color = NEUTRAL_COLOR_35
+export const DEFAULT_BACKGROUND_COLOR: Color = NEUTRAL_COLOR_10
 
-export const BRAND_COLOR_50: string = '#E80C7A'
+// Specific colors
+export const SUCCESS_COLOR: Color = '#53C000'
+export const WARNING_COLOR: Color = '#FFAD1D'
+export const FAIL_COLOR: Color = '#E33258'
